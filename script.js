04197fd5e186c7d533a0742e7f2c29e0b3159a3c
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-17 16:46:24 +0000

    Mad change

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var ctx = document.getElementById("canvas").getContext("2d");
-ctx.width = window.innerWidth;
-ctx.height = window.innerHeight;
 
-var x = 0;
-var y = 0;
 
-function drawCircle(x, y, radius, alpha)
-{
-  ctx.globalAlpha = alpha;
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-  ctx.fillStyle = 'white';
-  ctx.fill();
-}
-// were they bad?
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Charles: Pizzagamer#1302
+// Preston: bread.py#4577
+// Joel: paternaloil.js#                                     
